096e9d55d4061aa00e46aad2e1f4a4d978bbc550
anes89cok <anes89cok@gmail.com>
2015-03-03 15:44:56 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="845 South Main Street Suite 202"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Lombard";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica="IL"; // AKo ne znate skracenicu ostavite praznu
+	$telephone=6306207300;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
